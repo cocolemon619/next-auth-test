8d9ef918b46db6486ce6a3f10cc9be951536fade
cocolemon619 <torikuto.66@gmal.com>
2024-08-18 01:11:34 +0900

    設定ファイルの実装

---
+++ next-auth/src/middleware.ts
@@ -0,0 +1,10 @@
+// 設定を動作させるためのミドルウェア
+import NextAuth from "next-auth";
+import { authConfig } from '../auth.config'
+
+export default NextAuth(authConfig).auth
+
+export const config = {
+    // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+    matcher: ['/((?!api|_next/static|_next/image|.png).*)'],
+}
